3c0c9cd9ef7f4b3b8f134f93dfb1708e33306a87
JJ Ben-Joseph <jj@tensorspace.ai>
2023-10-30 16:12:42 -0400

    Add simple command line.

--- genegist/__main__.py
+++ genegist/__main__.py
@@ -1,2 +1,14 @@
+from argparse import ArgumentParser
+
+from genegist.data import GeneRIFS, gene2id
+
+
 def main():
-    print("Hello World!")
+    parser = ArgumentParser()
+    parser.add_argument("-g", "--gene", help="Look up GeneGists for a given gene")
+    args = parser.parse_args()
+
+    if args.gene:
+        gene_id = gene2id(args.gene)
+        generifs = GeneRIFS()
+        print(generifs.get_texts_by_gene_id(gene_id))
