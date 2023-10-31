176a1b7ba34b1137782ee901dd5920381b597fa6
JJ Ben-Joseph <jj@tensorspace.ai>
2023-10-31 15:24:48 -0400

    Add cmdline to create generifs for a geneset.

--- genegist/__main__.py
+++ genegist/__main__.py
@@ -5,10 +5,15 @@ from genegist.data import GeneRIFS, gene2id
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-g", "--gene", help="Look up GeneGists for a given gene")
+    parser.add_argument("-g", "--gene", help="Look up GeneRIFS for a given gene")
+    parser.add_argument("-s", "--geneset", help="Look up GeneRIFS for a given gene set")
     args = parser.parse_args()
 
     if args.gene:
         gene_id = gene2id(args.gene)
         generifs = GeneRIFS()
         print(generifs.get_texts_by_gene_id(gene_id))
+
+    if args.geneset:
+        generifs = GeneRIFS()
+        print(generifs.get_texts_by_gene_set(args.geneset))
