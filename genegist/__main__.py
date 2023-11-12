2a390743bbc2d170f8d5d43b11baf5c188787fc7
JJ Ben-Joseph <jj@tensorspace.ai>
2023-11-11 21:57:27 -0500

    Add command line.

--- genegist/__main__.py
+++ genegist/__main__.py
@@ -1,15 +1,21 @@
 from argparse import ArgumentParser
+import asyncio
 
+from genegist.analyzer import find_biological_process
 from genegist.data import GeneRIFS, gene2id
 
 
-def main():
+async def async_main():
     parser = ArgumentParser()
     parser.add_argument("-g", "--gene", help="Look up GeneRIFS for a given gene")
     parser.add_argument("-s", "--geneset", help="Look up GeneRIFS for a given gene set")
     parser.add_argument(
         "-a", "--abstracts", help="Also look up abstracts", action="store_true"
     )
+    parser.add_argument(
+        "-p", "--process", help="Find a biological process", action="store_true"
+    )
+
     args = parser.parse_args()
 
     if args.gene:
@@ -18,4 +24,13 @@ def main():
 
     if args.geneset:
         generifs = GeneRIFS()
-        print(generifs.get_texts_by_gene_set(args.geneset, args.abstracts))
+        texts = generifs.get_texts_by_gene_set(args.geneset, args.abstracts)
+        if not args.process:
+            print(texts)
+            return
+        else:
+            print(await find_biological_process(texts))
+
+
+def main():
+    asyncio.run(async_main())
