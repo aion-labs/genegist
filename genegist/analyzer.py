e926309722515be8ccc31d8ecfaed4f0373f4948
JJ Ben-Joseph <jj@tensorspace.ai>
2023-11-13 10:01:43 -0500

    Fix LLM module.

--- genegist/analyzer.py
+++ genegist/analyzer.py
@@ -1,22 +1,28 @@
 from typing import Dict
-
+import io
 from paperqa import Docs
 
 
 async def find_biological_process(data: Dict) -> str:
-    prompt = "Identify a subset of genes that represents a biological process, "
-    "and the role the genes in the process. You want to be as specific as possible."
-    "Ideally we want biological pathways."
+    prompt = (
+        "Identify a subset of genes that represents a biological process, "
+        "and the role the genes in the process. You want to be as specific as possible. "
+        "Ideally, we want biological pathways."
+    )
     index = Docs()
-    res = []
+
     for gene, rif in data.items():
+        # Process and clean up the text for each gene
         for i, r in enumerate(rif):
             if r[-1] not in [".", "?", "!"]:
                 rif[i] = r + "."
         rif = " ".join(rif)
         if rif.strip() == "":
             continue
-        index.add(f"For the gene {gene}: {rif}")
+
+        # Create an in-memory bytes-based file-like object for each gene's information
+        with io.BytesIO(f"For the gene {gene}: {rif}".encode()) as gene_file:
+            index.add_file(gene_file, dockey=gene)
 
     response = await index.aquery(prompt)
     return response.answer
