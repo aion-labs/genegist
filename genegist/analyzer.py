02ce390e9193f2cffc39ed97677a2d32fb0b7229
JJ Ben-Joseph <jj@tensorspace.ai>
2023-11-13 14:43:35 -0500

    Add dry run feature.

--- genegist/analyzer.py
+++ genegist/analyzer.py
@@ -3,13 +3,15 @@ import io
 from paperqa import Docs
 
 
-async def find_biological_process(data: Dict) -> str:
+async def find_biological_process(data: Dict, dry_run: bool = False) -> str:
     prompt = (
         "Identify a subset of genes that represents a biological process, "
         "and the role the genes in the process. You want to be as specific as possible. "
         "Ideally, we want biological pathways."
     )
     index = Docs()
+    if dry_run:
+        index = []
 
     for gene, rif in data.items():
         # Process and clean up the text for each gene
@@ -20,9 +22,16 @@ async def find_biological_process(data: Dict) -> str:
         if rif.strip() == "":
             continue
 
+        text = f"For the gene {gene}: {rif}"
+        if dry_run:
+            index.append(text)
+            continue
         # Create an in-memory bytes-based file-like object for each gene's information
-        with io.BytesIO(f"For the gene {gene}: {rif}".encode()) as gene_file:
+        with io.BytesIO(text.encode()) as gene_file:
             index.add_file(gene_file, dockey=gene)
+
+    if dry_run:
+        return "\n".join(index)
 
     response = await index.aquery(prompt)
     return response.answer
