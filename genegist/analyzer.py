1b3092e1782ff0236793b884c4154ae351cedc7f
JJ Ben-Joseph <jj@tensorspace.ai>
2023-11-16 21:51:19 -0500

    Add support for running LLMs on dry runs.

--- genegist/analyzer.py
+++ genegist/analyzer.py
@@ -1,34 +1,44 @@
-from typing import Dict
+from typing import Union, Dict
 import io
+from uuid import uuid4
 from paperqa import Docs
 
 
-async def find_biological_process(data: Dict, dry_run: bool = False) -> str:
+async def find_biological_process(data: Union[Dict, str], dry_run: bool = False) -> str:
     prompt = (
         "Identify a subset of genes that represents a biological process, "
         "and the role the genes in the process. You want to be as specific as possible. "
         "Ideally, we want biological pathways."
     )
     index = Docs()
+
     if dry_run:
         index = []
 
-    for gene, rif in data.items():
-        # Process and clean up the text for each gene
-        for i, r in enumerate(rif):
-            if r[-1] not in [".", "?", "!"]:
-                rif[i] = r + "."
-        rif = " ".join(rif)
-        if rif.strip() == "":
-            continue
+    if isinstance(data, str):
+        data = data.splitlines()
+        for line in data:
+            if line.strip() == "":
+                continue
+            with io.BytesIO(line.encode()) as gene_file:
+                index.add_file(gene_file, dockey=str(uuid4()))
+    else:
+        for gene, rif in data.items():
+            # Process and clean up the text for each gene
+            for i, r in enumerate(rif):
+                if r[-1] not in [".", "?", "!"]:
+                    rif[i] = r + "."
+            rif = " ".join(rif)
+            if rif.strip() == "":
+                continue
 
-        text = f"For the gene {gene}: {rif}"
-        if dry_run:
-            index.append(text)
-            continue
-        # Create an in-memory bytes-based file-like object for each gene's information
-        with io.BytesIO(text.encode()) as gene_file:
-            index.add_file(gene_file, dockey=gene)
+            text = f"For the gene {gene}: {rif}"
+            if dry_run:
+                index.append(text)
+                continue
+            # Create an in-memory bytes-based file-like object for each gene's information
+            with io.BytesIO(text.encode()) as gene_file:
+                index.add_file(gene_file, dockey=gene)
 
     if dry_run:
         return "\n".join(index)
